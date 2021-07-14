e81a781b31e9872d282b3cd68a230bc72503edfa
Denis Korzun <dzianiskor@gmail.com>
2021-07-14 14:40:38 +0300

    add template for Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { useRoutes } from "./routes";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./index.module.scss";
+import "./index.scss";
 
 function App() {
-  const isAuthenticated: boolean = true;
+  const isAuthenticated: boolean = false;
 
   const routes = useRoutes(isAuthenticated);
   return <Router>{routes}</Router>;
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import s from "./Login.module.scss";
 
 const LoginPage: React.FC = () => {
-  return <div>LoginPage</div>;
+  const [code, setCode] = useState(false);
+
+  const mockLogin = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setCode(true);
+    setTimeout(() => {
+      setCode(false);
+    }, 2000);
+  };
+
+  return (
+    <div
+      className={s.loginWrapper}
+      style={{ backgroundImage: `url("/img/login/login.jpg")` }}
+    >
+      {code ? (
+        <h1>Please wait...</h1>
+      ) : (
+        <a className={s.loginLink} href="/">
+          <button
+            className="btn btn-primary btn-lg"
+            onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+              mockLogin(e);
+            }}
+          >
+            <img src="/img/github.png" alt="cat" />
+            <span>Login with GitHub</span>
+          </button>
+        </a>
+      )}
+    </div>
+  );
 };
 
 export default LoginPage;
